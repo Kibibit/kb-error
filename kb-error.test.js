fbcf77bf54c4e9f2cf485372e7514b8d5584fdeb
Neil Kalman <Neilkalman@gmail.com>
2020-11-11 03:57:41 +0200

    test: jest changes + improvements + nits
    - Removed some type checks. For example: We're not actually testing the message 'type', so it can be something else than a string. for example, passing an array is totally acceptable as a message.
    
    - Added some extra checks for 'should be extendable'

--- kb-error.test.js
+++ kb-error.test.js
@@ -1,18 +1,17 @@
-const { expect } = require('chai');
-const kbError = require('./index');
+const kbError = require('./kb-error');
 
 describe('Test kbError', () => {
   it('Should be an Error', () => {
     const err = new kbError();
-    expect(err).to.be.an('error');
+    expect(err).toBeInstanceOf(Error);
   });
 
   it('Should be distinguishable from other errors', () => {
     const err = new kbError();
-    expect(err instanceof kbError).to.be.a('boolean').that.equals(true);
+    expect(err).toBeInstanceOf(kbError);
   });
 
-  it('Should be extenable', () => {
+  it('Should be extendable', () => {
     class FooError extends kbError {
       constructor(message) {
         super(message);
@@ -20,23 +19,26 @@ describe('Test kbError', () => {
     }
 
     const err = new FooError();
-    expect(err).to.be.an('error');
-    expect(err instanceof kbError).to.be.a('boolean').that.equals(true);
+    expect(err).toBeInstanceOf(Error);
+    expect(err).toBeInstanceOf(kbError);
+    expect(err).toBeInstanceOf(FooError);
+    expect(err.name).toBe('FooError');
   });
 
   it('Should have a "name" property', () => {
     const err = new kbError();
-    expect(err.name).to.be.a('string').that.equals('kbError');
+    expect(typeof err.name).toBe('string');
+    expect(err.name).toBe('kbError');
   });
 
   it('Should have a "stack"', () => {
     const err = new kbError();
-    expect(err.stack).to.exist;
+    expect(err.stack).toBeDefined();
   });
 
   it('Should use the "message" property provided to the constructor', () => {
     const msg = 'kibibit is awesome';
     const err = new kbError(msg);
-    expect(err.message).to.be.a('string').that.equal(msg);
+    expect(err.message).toBe(msg);
   });
 });
